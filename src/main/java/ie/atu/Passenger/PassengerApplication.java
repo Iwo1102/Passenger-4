36338f134b8ea19ae9e4ed35706a8d7612457e23
iwo <G00391231@atu.ie>
2022-12-18 17:03:58 +0000

    small fixes

--- src/main/java/ie/atu/Passenger/PassengerApplication.java
+++ src/main/java/ie/atu/Passenger/PassengerApplication.java
@@ -1,5 +1,6 @@
 package ie.atu.Passenger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,6 @@ import java.util.List;
 @RestController
 @SpringBootApplication
 @RequestMapping(path="api/passenger")
-
 public class PassengerApplication {
 
 	PassengerService myService;

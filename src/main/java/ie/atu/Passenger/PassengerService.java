5691cdbf16e7dc939fb6b20d8602bd69336d25f4
iwo <G00391231@atu.ie>
2022-12-18 20:45:07 +0000

    added find by age functionality

--- src/main/java/ie/atu/Passenger/PassengerService.java
+++ src/main/java/ie/atu/Passenger/PassengerService.java
@@ -37,4 +37,9 @@ public class PassengerService {
     {
         passengerRepo.deleteById(count);
     }
+
+    public List<Passenger> findPassengerByAgeRange(int age1, int age2)
+    {
+        return passengerRepo.findPassengerByAge(age1, age2);
+    }
 }
